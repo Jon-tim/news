f1cfbb9506fc9459bab190f32251c522775db7e7
jontim <awoniyitimi@outlook.com>
2022-10-24 18:38:30 +0100

    fix: Stop Mobile Nav from scrolling

--- src/typescript/main.js
+++ src/typescript/main.js
@@ -4,7 +4,9 @@ var closeNav = document.querySelector(".close");
 // function that brings mobile nav in
 openNav.addEventListener("click", function () {
     mobileNav.style.transform = "translateX(0%)";
+    document.body.style.overflowY = "hidden";
 });
 closeNav.addEventListener("click", function () {
     mobileNav.style.transform = "translateX(100%)";
+    document.body.style.overflowY = "unset";
 });
--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -5,7 +5,9 @@ const closeNav: any = document.querySelector(".close");
 // function that brings mobile nav in
 openNav.addEventListener("click", () => {
   mobileNav.style.transform = "translateX(0%)";
+  document.body.style.overflowY = "hidden";
 });
 closeNav.addEventListener("click", () => {
   mobileNav.style.transform = "translateX(100%)";
+  document.body.style.overflowY = "unset";
 });
