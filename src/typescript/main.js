fa0c162332633b16895ed56312573139e301d4b4
jontim <awoniyitimi@outlook.com>
2022-10-24 19:59:06 +0100

    feat: Add theme mode changes

--- src/typescript/main.js
+++ src/typescript/main.js
@@ -1,6 +1,7 @@
 var openNav = document.querySelector(".mobile-btn");
 var mobileNav = document.querySelector(".mobile-nav-section");
 var closeNav = document.querySelector(".close");
+var themeSwitch = document.querySelectorAll(".theme-switch-section i");
 // function that brings mobile nav in
 openNav.addEventListener("click", function () {
     mobileNav.style.display = "block";
@@ -9,4 +10,16 @@ openNav.addEventListener("click", function () {
 closeNav.addEventListener("click", function () {
     mobileNav.style.display = "none";
     document.body.style.overflowY = "unset";
+});
+//theme
+themeSwitch.forEach(function (element) {
+    element.addEventListener("click", function () {
+        document.body.classList.toggle("dark");
+        if (document.body.classList.contains("dark")) {
+            element.classList.replace("bx-moon", "bx-sun");
+        }
+        else {
+            element.classList.replace("bx-sun", "bx-moon");
+        }
+    });
 });
--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -1,6 +1,7 @@
 const openNav: any = document.querySelector(".mobile-btn");
 const mobileNav: any = document.querySelector(".mobile-nav-section");
 const closeNav: any = document.querySelector(".close");
+const themeSwitch: any = document.querySelectorAll(".theme-switch-section i");
 
 // function that brings mobile nav in
 openNav.addEventListener("click", () => {
@@ -10,4 +11,16 @@ openNav.addEventListener("click", () => {
 closeNav.addEventListener("click", () => {
   mobileNav.style.display = "none";
   document.body.style.overflowY = "unset";
+});
+
+//theme
+themeSwitch.forEach((element) => {
+  element.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    if (document.body.classList.contains("dark")) {
+      element.classList.replace("bx-moon", "bx-sun");
+    } else {
+      element.classList.replace("bx-sun", "bx-moon");
+    }
+  });
 });
